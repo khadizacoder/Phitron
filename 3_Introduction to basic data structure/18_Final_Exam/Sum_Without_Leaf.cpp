b1c8cb3de4e0d50f330689984ac1360f4b468f47
Khadiza Coder <freelancerkhadiza2@gmail.com>
2026-01-27 12:04:33 +0600

    binary tree

---
+++ 3_Introduction to basic data structure/18_Final_Exam/Sum_Without_Leaf.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void node_input(Node* root){
+    
+
+}
+
+int main()
+{
+    Node* root = node_input(root);
+    return 0;
+}
