5836b2ef0ff814ec0cb7d9218f66bbe2674e84f9
Khadiza Coder <freelancerkhadiza2@gmail.com>
2026-03-12 23:50:13 +0600

    assingment problem

--- 4_Algorithms/12_Assignment_2/Can_Go_Again.cpp
+++ 4_Algorithms/12_Assignment_2/Can_Go_Again.cpp
@@ -1,66 +1,91 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+class Edge
 {
-    int n, e;
-    cin >> n >> e;
-    long long int adjList[n + 1][n + 1];
-
-    for (int i = 1; i <= n; i++)
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
     {
-        for (int j = 1; j <= n; j++)
-        {
-            if (i == j)
-                adjList[i][j] = 0;
-            else
-                adjList[i][j] = LLONG_MAX;
-        }
+        this->a = a;
+        this->b = b;
+        this->c = c;
     }
+};
 
-    while (e--)
-    {
-        long long int a, b, c;
-        cin >> a >> b >> c;
-        adjList[a][b] = c;
-    }
+long long dis[1005];
+vector<Edge> edgeList;
+int n, e;
+bool cycle = false;
 
-    for (int k = 1; k <= n; k++)
+void bellmanFord()
+{
+    for (int i = 1; i <= n - 1; i++)
     {
-        for (int i = 1; i <= n; i++)
+        for (auto ed : edgeList)
         {
-            for (int j = 1; j <= n; j++)
+            int a, b;
+            long long int c;
+            a = ed.a;
+            b = ed.b;
+            c = ed.c;
+            if (dis[a] != LLONG_MAX && dis[a] + c < dis[b])
             {
-                if (adjList[i][k] != LLONG_MAX && adjList[k][j] != LLONG_MAX)
-                {
-                    long long int listSum = adjList[i][k] + adjList[k][j];
-                    if (listSum < adjList[i][j])
-                        adjList[i][j] = listSum;
-                }
+                dis[b] = dis[a] + c;
             }
         }
     }
 
-    bool cycle = false;
-    for (int i = 1; i <= n; i++)
-        if (adjList[i][i] < 0)
-            cycle = true;
-
-    if (cycle)
+    for (auto ed : edgeList)
     {
-        cout << "Negative Cycle Detected\n";
-        return 0;
+        int a, b;
+        long long int c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+        if (dis[a] != LLONG_MAX && dis[a] + c < dis[b])
+        {
+            cycle = true;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        edgeList.push_back(Edge(a, b, w));
     }
 
-    int s, t;
-    cin >> s >> t;
-    while (t--)
+    for (int i = 1; i <= n; i++)
+        dis[i] = LLONG_MAX;
+
+    int src, tc;
+    cin >> src >> tc;
+
+    dis[src] = 0;
+    bellmanFord();
+
+    while (tc--)
     {
-        int d;
-        cin >> d;
-        if (adjList[s][d] == LLONG_MAX)
-            cout << "Not Possible\n";
+        int des;
+        cin >> des;
+        if (cycle)
+        {
+            cout << "Negative Cycle Detected\n";
+            break;
+        }
         else
-            cout << adjList[s][d] << endl;
+        {
+            if (dis[des] == LLONG_MAX)
+                cout << "Not Possible\n";
+            else
+                cout << dis[des] << endl;
+        }
     }
 
     return 0;
