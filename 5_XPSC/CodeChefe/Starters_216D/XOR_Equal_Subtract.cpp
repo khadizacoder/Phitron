d159ba77e227311dfa6801833d8233b46254970e
Khadiza Coder <freelancerkhadiza2@gmail.com>
2026-06-17 14:22:49 +0600

    Add accepted solution for D Number

--- 5_XPSC/CodeChefe/Starters_216D/XOR_Equal_Subtract.cpp
+++ 5_XPSC/CodeChefe/Starters_216D/XOR_Equal_Subtract.cpp
@@ -14,7 +14,33 @@ int main()
 {
     fastIO();
 
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(all(a));
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if((a[j] & a[i]) == a[j])
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+
+        cout << ans << nl;
+    }
 
     return 0;
 }
