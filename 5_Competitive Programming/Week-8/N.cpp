83c69b0ef1ee920c08ec12c710d74086f41b705a
Khadiza Coder <freelancerkhadiza2@gmail.com>
2026-07-05 17:38:56 +0600

    Week 8 - Last problem solve done

--- 5_Competitive Programming/Week-8/N.cpp
+++ 5_Competitive Programming/Week-8/N.cpp
@@ -17,7 +17,15 @@ int main()
     int t; cin >> t;
     while(t--)
     {
-        
+        int l, r; cin >> l >> r;
+
+        if(l*2 <= r)
+        {
+            cout << l << " " << l*2 << nl;
+        }
+        else{
+            cout << -1 << " " << -1 << nl;
+        }
     }
 
     return 0;
